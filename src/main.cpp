083a18df30d74182f7341e93f09c635058c6a355
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-10-16 18:51:29 +0200

    Debugging chapter Learncpp.com

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
-#include "io.h"
 
-int main() {
-
-    int x = io::readNumber();
-    int y = io::readNumber();
-
-    io::writeAnswer(x + y);
-
-    return 0;
+int readNumber()
+{
+	std::cout << "Please enter a number: ";
+	int x;
+    std::cin >> x;
+	return x;
 }
 
+void writeAnswer(int x)
+{
+	std::cout << "The sum is: " << x << '\n';
+}
+
+int main()
+{
+	int x { readNumber() };
+	x = x + readNumber();
+	writeAnswer(x);
+
+	return 0;
+}
