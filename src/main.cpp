f9030c8ec6e46739578b0515269d5d5141409627
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-10-18 21:57:47 +0200

    Signed and Unsigned int operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,14 @@
-#include <iomanip> // for std::setw (which sets the width of the subsequent output)
 #include <iostream>
-#include <climits> // for CHAR_BIT
 
 int main()
 {
-    std::cout << "A byte is " << CHAR_BIT << " bits\n\n";
 
-    std::cout << std::left; // left justify output
+    // assume 4 byte integers
+    int x { 2'147'483'647 }; // the maximum value of a 4-byte signed integer
+    std::cout << x << '\n';
 
-    std::cout << std::setw(16) << "bool:" << sizeof(bool) << " bytes\n";
-    std::cout << std::setw(16) << "char:" << sizeof(char) << " bytes\n";
-    std::cout << std::setw(16) << "short:" << sizeof(short) << " bytes\n";
-    std::cout << std::setw(16) << "int:" << sizeof(int) << " bytes\n";
-    std::cout << std::setw(16) << "long:" << sizeof(long) << " bytes\n";
-    std::cout << std::setw(16) << "long long:" << sizeof(long long) << " bytes\n";
-    std::cout << std::setw(16) << "float:" << sizeof(float) << " bytes\n";
-    std::cout << std::setw(16) << "double:" << sizeof(double) << " bytes\n";
-    std::cout << std::setw(16) << "long double:" << sizeof(long double) << " bytes\n";
+    x = x + 1; // integer overflow, undefined behavior
+    std::cout << x << '\n';
 
     return 0;
 }
