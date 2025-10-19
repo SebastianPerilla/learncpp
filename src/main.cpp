eeff8c3bb6f4f492d9ccde8e75b2dd2d4cad07d6
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-10-19 21:32:05 +0200

    Adding in Scientific Notation and Floating Point Eval chapter

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
+#include <cstddef>  // for std::size_t
 #include <iostream>
+#include <vector>
+
 
 int main()
 {
 
-    // assume 4 byte integers
-    int x { 2'147'483'647 }; // the maximum value of a 4-byte signed integer
-    std::cout << x << '\n';
+    std::vector<float> x = {1e0,
+                            1e1,
+                            1e2,
+                            1e3,
+                            1e4,
+                            1e5};
 
-    x = x + 1; // integer overflow, undefined behavior
-    std::cout << x << '\n';
+    for (int i = 0; i < x.size(); i++) {
+        std::cout << x[i] << '\n';
+    }
+
+    // std::cout << x.size() << '\n';
 
     return 0;
 }
