3aa6999bda35a55eeb8e5e02019e66040bc32402
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-11-08 23:42:46 +0100

    Examples or using current concepts

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <cmath>
 
-bool isAllowedToTakeFunRide(){
-    std::cout << "What is your height?: ";
-    double height;
-    std::cin >> height;
-    
-    return height >= 140.0;
+void fallen(int height, int seconds)
+{
+
+	double gravityConstant{9.8};
+
+	double distanceFallen{gravityConstant * (seconds * seconds) / 2};
+
+	double result = height - distanceFallen;
+
+	if (result < 0.0)
+	{
+		std::cout << "At "<< seconds << " seconds the ball is on the ground\n";
+	}
+	else
+	{
+
+		std::cout << "At " << seconds << " the ball it at height: " << height - distanceFallen << " " << height << " Meters\n";
+	}
 }
 
-int main(){
-    std::cout << "Testing out Escape Sequences";
+int main()
+{
 
-    std::cout << "Trying out the \', now the \", now the \\ , now the \t, \? escape sequence";
-    return 0;
+	std::cout << "Enter the height of the tower in meters: ";
+
+	int height;
+	std::cin >> height;
+
+	fallen(height, 0);
+	fallen(height, 1);
+	fallen(height, 2);
+	fallen(height, 3);
+	fallen(height, 4);
+ 	fallen(height, 5);
+
+	return 0;
 }
