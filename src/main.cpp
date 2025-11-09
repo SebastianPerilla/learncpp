9dad703327556c1174523bd47a53399b431a9ab3
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-11-09 20:19:49 +0100

    Using constant variables now

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,12 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
-void fallen(int height, int seconds)
-{
 
-	double gravityConstant{9.8};
+int main() {
+	
+	const double gravity { 9.18 };
 
-	double distanceFallen{gravityConstant * (seconds * seconds) / 2};
-
-	double result = height - distanceFallen;
-
-	if (result < 0.0)
-	{
-		std::cout << "At "<< seconds << " seconds the ball is on the ground\n";
-	}
-	else
-	{
-
-		std::cout << "At " << seconds << " the ball it at height: " << height - distanceFallen << " " << height << " Meters\n";
-	}
-}
-
-int main()
-{
-
-	std::cout << "Enter the height of the tower in meters: ";
-
-	int height;
-	std::cin >> height;
-
-	fallen(height, 0);
-	fallen(height, 1);
-	fallen(height, 2);
-	fallen(height, 3);
-	fallen(height, 4);
- 	fallen(height, 5);
+	std::cout << gravity << "\n";
 
 	return 0;
 }
