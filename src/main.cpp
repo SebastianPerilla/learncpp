7592d510ac03b953292314460f0d712bd0bf5327
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-11-12 23:22:05 +0100

    Working with sorting algorithms and array containers

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
+#include <vector>
 #include <iostream>
-#include <string>
+#include <algorithm>
+#include <array>
+#include <iterator>
 
+int main()
+{
 
-int main() {
-	
-	const double gravity { 9.18 };
+	std::array<int, 12> arr = {4, 1, 4, 4, 2, 3, 5, 6, 7, 8, 9, 10};
 
-	std::cout << gravity << "\n";
+	std::sort(arr.begin(), arr.end());
 
+	for (auto i : arr)
+	{
+		std::cout << i << " ";
+	}
 	return 0;
 }
