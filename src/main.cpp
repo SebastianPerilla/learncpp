9ea28472a2d54ec9d93d458992453c7c44bcf742
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-11-18 23:46:34 +0100

    Working with std::string and getLine

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 #include <string>
 
-std::string getName()
-{
-	std::cout << "Enter your name: ";
-	std::string name;
-	std::getline(std::cin >> std::ws, name);
-	return name;
-}
 
-int getAge()
-{
-
-	std::cout << "Enter your age: ";
-	int age;
-	std::cin >> age;
-	return age;
-}
 
 int main()
 {
-	getName();
-	getAge();
+	std::cout << "Enter your name: ";
+	std::string name {};
+	std::getline(std::cin >> std::ws, name);
+	
+	std::cout << "Enter your age: ";
+	int age;
+	std::cin >> age;
+	
+	int nameLen = static_cast<int>(name.length());;
+
+	std::cout << "Your age + length of name is: " <<  nameLen + age; 
 	
 	return 0;
 }
