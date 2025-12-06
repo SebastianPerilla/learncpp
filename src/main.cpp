fd400ad56416eb1741381c226a1c367348f04ac5
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-06 23:39:22 +0100

    Internal Linkage

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
-#include <iostream>
 
 int main()
-{ // outer block
-    int apples { 5 }; // here's the outer block apples
+{ 
+    static int x{10};
 
-    { // nested block
-        // apples refers to outer block apples here
-        std::cout << apples << '\n'; // print value of outer block apples
-
-        int innerApples{ 0 }; // define apples in the scope of the nested block
-
-        // apples now refers to the nested block apples
-        // the outer block apples is temporarily hidden
-
-        innerApples = 10; // this assigns value 10 to nested block apples, not outer block apples
-
-        std::cout << innerApples << '\n'; // print value of nested block apples
-    } // nested block apples destroyed
-
-
-    std::cout << apples << '\n'; // prints value of outer block apples
+    std::cout << "Using Internal Linkage!: ";
+    std::cout << x;
 
     return 0;
-} // outer block apples destroyed
+} 
