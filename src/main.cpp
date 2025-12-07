ca5329382c233a01edc38f6536464b631dcc9c47
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-07 19:02:42 +0100

    Global Linkage

--- src/main.cpp
+++ src/main.cpp
@@ -9,3 +9,16 @@ int main()
 
     return 0;
 } 
+
+
+
+/*
+Scope: Where a variable is accessible
+Duration: When a variable is created and destroyed
+Linkage: whether the variable can be exported to another file or not.
+
+Global Variables:
+Scope: Global Accessible Everywhere
+Duration: (static/internal linkage) so they terminate when the program terminates
+Variables: can have both internal and external linkage (static vs extern keywords)
+*/
