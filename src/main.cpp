24954b2ec662ca898603eec9568218344e74129a
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-08 17:58:07 +0100

    Static Local Variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 
-int main()
-{ 
-    static int x{10};
+void incrementAndPrint()
+{
+    static int value{ 1 }; // automatic duration by default
+    ++value;
+    std::cout << value << '\n';
+} // value is destroyed here
 
-    std::cout << "Using Internal Linkage!: ";
-    std::cout << x;
+int main()
+{
+    incrementAndPrint();
+    incrementAndPrint();
+    incrementAndPrint();
 
     return 0;
-} 
-
+}
 
 
 /*
-Scope: Where a variable is accessible
-Duration: When a variable is created and destroyed
-Linkage: whether the variable can be exported to another file or not.
+What effect does using keyword static have on a global variable? 
 
 Global Variables:
-Scope: Global Accessible Everywhere
-Duration: (static/internal linkage) so they terminate when the program terminates
-Variables: can have both internal and external linkage (static vs extern keywords)
+Static by default and make the variable have internal linkage (which it has by default)
+meaning the variable cannot be exported to other files
+
+
+What effect does it have on a local variable?
+Changes the variable from automatic duration (meaning its duration is from the 
+point of its defintion to the end of the block its in) to static (lasts from the beginning of 
+the program until the end)
+ 
+
+
 */
