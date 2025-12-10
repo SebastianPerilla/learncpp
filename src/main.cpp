a61cbe65e9d6e757ddcbe56b4741935a23598aaa
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-10 18:24:00 +0100

    Question 3 Chapter 7 Quiz

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
-#include "constants.h"
 #include <iostream>
+
+int accumulate(int x)
+{
+    static int result{ 0 };
+    return result += x;
+}
 
 int main()
 {
-	std::cout << "How many students are in your class? ";
-	int students{};
-	std::cin >> students;
+    std::cout << accumulate(4) << '\n'; // prints 4
+    std::cout << accumulate(3) << '\n'; // prints 7
+    std::cout << accumulate(2) << '\n'; // prints 9
+    std::cout << accumulate(1) << '\n'; // prints 10
 
-
-	if (students > Constants::maxClassSize)
-		std::cout << "There are too many students in this class";
-	else
-		std::cout << "This class isn't too large";
-
-	return 0;
+    return 0;
 }
+
+/*
+Two Shortcomings of accumulate Extra Credit:
+- Cannot be reset due to its static duration
+- Cannot have multiple accumulators, as you will get unexpected answers if running together
+*/
