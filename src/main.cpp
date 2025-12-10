0f9ad45d5d1ba228e3fc28446a628758b37229a1
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-10 18:13:59 +0100

    Correcting Chapter 7 Summary

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,18 @@
 #include <iostream>
 
-void incrementAndPrint()
-{
-    static int value{ 1 }; // automatic duration by default
-    ++value;
-    std::cout << value << '\n';
-} // value is destroyed here
-
 int main()
 {
-    incrementAndPrint();
-    incrementAndPrint();
-    incrementAndPrint();
+    std::cout << "Enter a positive number: ";
+    int num{};
+    std::cin >> num;
+
+
+    if (num < 0) {
+        std::cout << "Negative number entered.  Making positive.\n";
+        num = -num;
+    }
+
+    std::cout << "You entered: " << num;
 
     return 0;
 }
-
-
-/*
-What effect does using keyword static have on a global variable? 
-
-Global Variables:
-Static by default and make the variable have internal linkage (which it has by default)
-meaning the variable cannot be exported to other files
-
-
-What effect does it have on a local variable?
-Changes the variable from automatic duration (meaning its duration is from the 
-point of its defintion to the end of the block its in) to static (lasts from the beginning of 
-the program until the end)
- 
-
-
-*/
