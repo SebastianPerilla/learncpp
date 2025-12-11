db1e6c80bd07eb7c3c249672a97843e152b3a190
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-11 14:18:05 +0100

    Switch Statements and Switch Fallthrough/Scoping

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,55 @@
 #include <iostream>
 
-int accumulate(int x)
+int calculate(int a, int b, char c)
 {
-    static int result{ 0 };
-    return result += x;
+    switch (c)
+    {
+    case '+':
+        return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        return a / b;
+    case '%':
+        return a % b;
+    default :
+        std::cout << "Error, non valid operator";
+        return 0;
+    }
+
 }
+
 
 int main()
 {
-    std::cout << accumulate(4) << '\n'; // prints 4
-    std::cout << accumulate(3) << '\n'; // prints 7
-    std::cout << accumulate(2) << '\n'; // prints 9
-    std::cout << accumulate(1) << '\n'; // prints 10
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
 
+    std::cout << "Enter another integer: ";
+    int y{};
+    std::cin >> y;
+
+    std::cout << "Enter a mathematical operator (+, -, *, /, or %): ";
+    char op{};
+    std::cin >> op;
+
+    std::cout << calculate(x,y,op);
     return 0;
 }
 
+
 /*
-Two Shortcomings of accumulate Extra Credit:
-- Cannot be reset due to its static duration
-- Cannot have multiple accumulators, as you will get unexpected answers if running together
+    Write a function called calculate() that takes two integers and a char
+    representing one of the following mathematical
+    operations: +, -, *, /, or % (remainder).
+
+    Use a switch statement to perform the appropriate mathematical
+    operation on the integers, and return the result. If an invalid operator
+    is passed into the function, the function should print an error message.
+
+    For the division operator, do an integer division, and don’t worry
+    about divide by zero.
 */
