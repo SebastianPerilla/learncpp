f5e03450b391fee0fb72074019c61ba1550db620
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-19 13:08:17 +0100

    For Loop Final question

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 
-int main() {
-  
-  for (int i {1}; i <= 20; ++i) {
-    if (i%2 == 0) {
-      std::cout << i << "\n";
-    }
-  }
+void fizzbuzz(int count){
+  for (int i{ 1 }; i <= count; ++i)
+	{
+		bool printed{ false };
+		if (i % 3 == 0)
+		{
+			std::cout << "fizz";
+			printed = true;
+		}
+		if (i % 5 == 0)
+		{
+			std::cout << "buzz";
+			printed = true;
+		}
+		if (i % 7 == 0)
+		{
+			std::cout << "pop";
+			printed = true;
+		}
 
+		if (!printed)
+			std::cout << i;
+
+		std::cout << '\n';
+  }
+}
+
+int main() {
+  fizzbuzz(150);
   return 0;
 }
