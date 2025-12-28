0295f033b96325eb716ca6731c98ee51a76399a2
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-27 23:58:06 -0600

    Periodicity

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 
 int main(){
-
-  std::cout << "Random Number Generator\n";
-  
-  std::cout << "For one of these we need the following:\n";
-  std::cout << " - Seed(Normally taken from our device)";
-  std::cout << " - Generator";
-  std::cout << " - A Distribution (Uniform, gaussian)";
+  std::cout << "Random Number Generators";
+  std::cout << "Periodicity, Seeding, Uniformity, dimensional distribution, high and low periods";
 
   return 0;
 }
