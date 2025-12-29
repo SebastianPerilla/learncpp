b47a54f002995ad00eaf4a2dbc6b77c8e362d844
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-28 18:29:36 -0600

    Clock Seeding Mersenne Twister

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
+#include <chrono>
 #include <iostream>
+#include <random>
 
 int main(){
-  std::cout << "Random Number Generators";
-  std::cout << "Periodicity, Seeding, Uniformity, dimensional distribution, high and low periods";
+  std::cout << "Mersenne Twister with Clock Seed\n";
+
+  std::mt19937_64 mt{ static_cast<std::mt19937_64::result_type>(std::chrono::steady_clock::now().time_since_epoch().count())};
+
+  std::uniform_int_distribution die6{1,6};
+
+  for (int count{1}; count <= 40; ++count) {
+    std::cout << die6(mt) << "\t";
+
+    if (count % 10 == 0){
+      std::cout << "\n";
+    }
+  }
+  
 
   return 0;
 }
