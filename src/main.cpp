20c0ed67873982be5e1963618574c18ccbf6770c
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-12-30 18:52:10 -0600

    Seeding with the random device

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
-#include <chrono>
 #include <iostream>
 #include <random>
 
-int main(){
-  std::cout << "Mersenne Twister with Clock Seed\n";
+int main()
+{
+  std::cout << "Seeding with random device";
+	std::mt19937 mt{ std::random_device{}() };
 
-  std::mt19937_64 mt{ static_cast<std::mt19937_64::result_type>(std::chrono::steady_clock::now().time_since_epoch().count())};
+	// Create a reusable random number generator that generates uniform numbers between 1 and 6
+	std::uniform_int_distribution die6{ 1, 6 }; // for C++14, use std::uniform_int_distribution<> die6{ 1, 6 };
 
-  std::uniform_int_distribution die6{1,6};
+	// Print a bunch of random numbers
+	for (int count{ 1 }; count <= 40; ++count)
+	{
+		std::cout << die6(mt) << '\t'; // generate a roll of the die here
 
-  for (int count{1}; count <= 40; ++count) {
-    std::cout << die6(mt) << "\t";
+		// If we've printed 10 numbers, start a new row
+		if (count % 10 == 0)
+			std::cout << '\n';
+	}
 
-    if (count % 10 == 0){
-      std::cout << "\n";
-    }
-  }
-  
-
-  return 0;
+	return 0;
 }
