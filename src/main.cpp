16bba17e05ce9637b0101c97d48214d63bba7602
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2026-01-30 17:03:39 +0100

    Arithmetic Conversions

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,7 @@
 #include <iostream>
-
+#include <typeinfo>
 int main() {
-    std::cout << "check";
+    auto x { 2 + 3.5 };
+    std::cout << typeid(x).name();
     return 0;
 }
-// struct User {
-//         std::string name;
-//         int *score; // heap allocated score
-//
-//         User(const std::string &n, int s) : name(n) { score = new int(s); }
-//         ~User() { delete score; }
-// };
-//
-// std::vector<User> loadUsers() {
-//     std::vector<User> users;
-//     users.reserve(3);
-//
-//     users.push_back(User("Alice", 10));
-//     users.push_back(User("Bob", 20));
-//     users.push_back(User("Cara", 30));
-//
-//     return users;
-// }
-//
-// int totalScore(std::vector<User> users) { // takes by value
-//     int total = 0;
-//     for (auto u : users) { // copies each User
-//         total += *u.score;
-//     }
-//     return total;
-// }
-//
-// int main() {
-//     auto users = loadUsers();
-//
-//     std::cout << "Total: " << totalScore(users) << "\n";
-//
-//     // update Bob
-//     for (auto u : users) {
-//         if (u.name == "Bob") {
-//             *u.score += 100;
-//         }
-//     }
-//
-//     std::cout << "Total after update :" << totalScore(users) << "\n";
-// }
