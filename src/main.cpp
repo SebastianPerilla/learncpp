667775b27ad2e9af0e8f711aad8ad9fc4046e9a8
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2026-02-14 18:05:36 +0100

    Template Parameters and Non-type template parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
 
-int func(int value, int value1) { return value * value1; }
+template <int T>
+constexpr int factorial() {
 
-int func(double value, double value1) { return value * value1; }
+    static_assert(T >= 0);
+
+    int result { 1 };
+    for (int i { 2 }; i <= T; ++i) {
+        result *= i;
+    }
+    return result;
+}
 
 int main() {
-    // std::cout << func(1) << "\n";
-    std::cout << func(1, 5);
+    static_assert(factorial<0>() == 1);
+    static_assert(factorial<3>() == 6);
+    static_assert(factorial<5>() == 120);
+
+    factorial<-3>(); // should fail to compile
     return 0;
 }
