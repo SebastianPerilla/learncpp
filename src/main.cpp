cbd68b28f23f88632a37f92669490683be318618
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2026-02-15 23:26:25 +0100

    Adding in template

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,14 @@
+#include <iostream>
 
-template <int T>
-constexpr int factorial() {
-
-    static_assert(T >= 0);
-
-    int result { 1 };
-    for (int i { 2 }; i <= T; ++i) {
-        result *= i;
-    }
-    return result;
+template <typename T, typename U>
+auto sub(T a, U b) {
+    return a - b;
 }
 
 int main() {
-    static_assert(factorial<0>() == 1);
-    static_assert(factorial<3>() == 6);
-    static_assert(factorial<5>() == 120);
+    std::cout << sub(3, 2) << '\n';
+    std::cout << sub(3.5, 2) << '\n';
+    std::cout << sub(4, 1.5) << '\n';
 
-    factorial<-3>(); // should fail to compile
     return 0;
 }
