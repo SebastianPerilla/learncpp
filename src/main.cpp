a8b290a2cb1a613f880b3b4bbfa4934054753aba
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2026-02-18 16:32:44 +0100

    Constexpr quix

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,49 @@
 #include <iostream>
 
-template <typename T, typename U>
-auto sub(T a, U b) {
-    return a - b;
+// gets tower height from user and returns it
+double getTowerHeight() {
+    std::cout << "Enter the height of the tower in meters: ";
+    double towerHeight {};
+    std::cin >> towerHeight;
+    return towerHeight;
+}
+
+// Returns ball height from ground after "seconds" seconds
+constexpr double calculateBallHeight(const double towerHeight, const int seconds) {
+    constexpr double gravity { 9.8 };
+
+    // Using formula: [ s = u * t + (a * t^2) / 2 ], here u(initial velocity) = 0
+    const double distanceFallen { (gravity * (seconds * seconds)) / 2.0 };
+    const double currentHeight { towerHeight - distanceFallen };
+
+    return currentHeight;
+}
+
+// Prints ball height above ground
+void printBallHeight(const double ballHeight, const int seconds) {
+    if (ballHeight > 0.0) {
+        std::cout << "At " << seconds << " seconds, the ball is at height: " << ballHeight << " meters\n";
+    } else {
+        std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
+    }
+}
+
+// Calculates the current ball height and then prints it
+// This is a helper function to make it easier to do this
+void printCalculatedBallHeight(double towerHeight, int seconds) {
+    const double ballHeight { calculateBallHeight(towerHeight, seconds) };
+    printBallHeight(ballHeight, seconds);
 }
 
 int main() {
-    std::cout << sub(3, 2) << '\n';
-    std::cout << sub(3.5, 2) << '\n';
-    std::cout << sub(4, 1.5) << '\n';
+    const double towerHeight { getTowerHeight() };
+
+    printCalculatedBallHeight(towerHeight, 0);
+    printCalculatedBallHeight(towerHeight, 1);
+    printCalculatedBallHeight(towerHeight, 2);
+    printCalculatedBallHeight(towerHeight, 3);
+    printCalculatedBallHeight(towerHeight, 4);
+    printCalculatedBallHeight(towerHeight, 5);
 
     return 0;
 }
